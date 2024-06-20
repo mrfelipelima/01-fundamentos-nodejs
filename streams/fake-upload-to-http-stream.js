ef7e1fbe1d862120d10b159a22ff632f4062dd5c
Felipe Lima <mrfelipelima@gmail.com>
2024-06-20 12:11:35 -0300

    Consumindo uma stream completa

--- streams/fake-upload-to-http-stream.js
+++ streams/fake-upload-to-http-stream.js
@@ -21,4 +21,8 @@ fetch('http://localhost:3334', {
   method: 'POST',
   body: new OneToOneHundredStream(),
   duplex: 'half',
+}).then(response => {
+  return response.text()
+}).then(data => {
+  console.log(data)
 })
