0b4f5d56b6f876a8cb8299a592ce3b7855f42d81
Felipe Lima <mrfelipelima@gmail.com>
2024-06-20 12:34:05 -0300

    Entendendo Buffers no Node

---
+++ streams/buffer.js
@@ -0,0 +1,8 @@
+/**
+ * O buffer é uma maneira que o node tem de trabalhar com dados binários que
+ * tem o processamento mais rápido.
+ */
+
+const buf = Buffer.from("Hello World")
+
+console.log(buf)
